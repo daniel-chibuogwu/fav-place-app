cef6be29cdcb0c5ae702b31e4d761c0cf1903396
dan-chibuogwu17 <danielchibuogwu@gmail.com>
2022-09-27 01:53:05 +0100

    added image picker

---
+++ components/Places/PlaceForm.js
@@ -0,0 +1,49 @@
+import { ScrollView, Text, TextInput, View, StyleSheet } from "react-native";
+import { useState } from "react";
+import { Colors } from "../../constants/colors";
+import ImagePicker from "./ImagePicker";
+
+function PlaceForm() {
+  const [enteredTitle, setEnteredTitle] = useState("");
+
+  function changeTitleHandler(enteredTitle) {
+    setEnteredTitle(enteredTitle);
+  }
+
+  return (
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={changeTitleHandler}
+          value={enteredTitle}
+        />
+        <ImagePicker />
+      </View>
+    </ScrollView>
+  );
+}
+
+export default PlaceForm;
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: "bold",
+    marginBottom: 4,
+    color: Colors.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: Colors.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: Colors.primary100,
+  },
+});
