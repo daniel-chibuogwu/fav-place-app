902acb9220d1f9d9576e4ed76b094e040365266b
dan-chibuogwu17 <danielchibuogwu@gmail.com>
2022-10-08 01:13:18 +0100

    using SQLite to save data to my native device database

---
+++ util/database.js
@@ -0,0 +1,23 @@
+import * as SQLite from "expo-sqlite";
+
+const database = SQLite.openDatabase("places.db");
+
+export function init() {
+  const promise = new Promise((resolve, reject) => {
+    database.transaction((tx) => {
+      tx.executeSql(
+        `CREATE TABLE IF NOT EXISTS places (
+    id INTEGER PRIMARY KEY NOT NULL,
+    title TEXT NOT NULL,
+    imageUri TEXT NOT NULL,
+    address TEXT NOT NULL,
+    lat REAL NOT NULL,
+    lng REAL NOT NULL
+    )`,
+        [],
+        () => {},
+        (_, error) => {}
+      );
+    });
+  });
+}
